ecb76aafa05a91deac6f52b031bab2eb435a5baa
Zaydguendouze <z.guendouze@outlook.fr>
2022-07-27 15:32:35 +0200

    Accessibilité page d'accueil

--- scripts/factories/Photographer.js
+++ scripts/factories/Photographer.js
@@ -18,12 +18,12 @@ function photographFactory(data) {
     function photographerCard(data) {
         const { name, id, city, country, tagline, price, portrait } = data
         return `
-                <a>
-                    <img src="assets/photographers/${portrait}"
+                <a href="photographer.html?id=${id}" aria-label="Accéder à la page du photographe ${name}" tabindex="0">
+                    <img src="assets/photographers/${portrait}" alt="Image du photographe ${name}"
                     />
                 <h2>${name}</h2>
                 </a>
-                <div class="infos_photographer">
+                <div class="infos_photographer" aria-label="infos du photographe ${name}">
                     <h3>${city}, ${country}</h3>
                     <p>${tagline}</p>
                     <p>${price}€/jour</p>
